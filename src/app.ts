d5ee90d65c189bf6ddb0e0347667190803c0f4fd
lisknonanika <lisknonanika@gmail.com>
2021-08-26 19:03:31 +0900

    upd

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,7 @@ const updateJob:CronJob = new CronJob('0 0 */6 * * 1-5', async() => await update
 (async() => {
   try {
     if (!sendJob.running) sendJob.start();
-    if (!updateJob.running && !sendJob.running) updateJob.start();
+    if (!updateJob.running) updateJob.start();
 
   } catch(err) {
     console.error(err);
