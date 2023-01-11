ed4a649e6ec30d4abaf09946dd9a7b38c2e6e1d8
lisknonanika <lisknonanika@gmail.com>
2023-01-11 12:11:50 +0900

    upd

--- src/action/sendReward.ts
+++ src/action/sendReward.ts
@@ -1,9 +1,9 @@
 import mysql from 'mysql2/promise';
-import { getMyAccount, getTransferTransaction, transfer } from '../common/lisk';
+import { getMyAccount, isTargetTransfer, transfer } from '../common/lisk';
 import { findRewardTargetVoters, updVoter } from '../common/mysql';
 import { DELEGATE } from '../common/config';
 
-export const sendReward = async(mysqlConnection:mysql.Connection):Promise<boolean> => {
+export const sendReward = async (mysqlConnection: mysql.Connection): Promise<boolean> => {
   try {
     console.info(`[sendReward] Start`);
 
@@ -13,11 +13,14 @@ export const sendReward = async(mysqlConnection:mysql.Connection):Promise<boolea
 
     // Get: delegate account
     const account = await getMyAccount();
-    const nonce:string = account.sequence.nonce;
-    let newNonce:string = nonce;
+    const nonce: string = account.sequence.nonce;
+    let newNonce: string = nonce;
 
     // Main 1
     for (const voter of voterRows) {
+      // Check: isTarget
+      if (!await isTargetTransfer(DELEGATE.ADDRESS, voter.address)) continue;
+
       // Transfer: reward
       if (!await transfer(newNonce, voter.address, voter.reward, DELEGATE.MESSAGE)) {
         console.error(`[sendReward] transfer failed: address=${voter.address}, reward=${voter.reward}`);
@@ -26,22 +29,22 @@ export const sendReward = async(mysqlConnection:mysql.Connection):Promise<boolea
 
       // Add newNonce
       newNonce = (BigInt(newNonce) + BigInt(1)).toString();
-      
+
       // sleep 1 sec.
       await new Promise(resolve => setTimeout(resolve, 1000));
     }
-      
+
     // sleep 30 sec.
     await new Promise(resolve => setTimeout(resolve, 30000));
 
     // Main 2
     for (const voter of voterRows) {
-      const data = await getTransferTransaction(DELEGATE.ADDRESS, voter.address);
-      if (!data || BigInt(nonce) > BigInt(data[0].nonce) || BigInt(newNonce) <= BigInt(data[0].nonce)) continue;
+      // Check: isTarget
+      if (await isTargetTransfer(DELEGATE.ADDRESS, voter.address)) continue;
 
       // Update: voter
       await updVoter(mysqlConnection, true, { id: voter.id, address: voter.address, reward: "0" });
-      
+
       // sleep 1 sec.
       await new Promise(resolve => setTimeout(resolve, 1000));
     }
--- src/common/lisk.ts
+++ src/common/lisk.ts
@@ -2,7 +2,7 @@ import fetch from 'node-fetch';
 import { NETWORK, API_MY_URL, API_RETRY_URL, DELEGATE } from './config';
 import { cryptography, transactions } from '@liskhq/lisk-client';
 
-export const getMyAccount = async():Promise<any> => {
+export const getMyAccount = async (): Promise<any> => {
   try {
     const response = await fetch(`${API_MY_URL[NETWORK]}/accounts?username=${DELEGATE.NAME}&isDelegate=true&limit=1&offset=0`);
     const data = (await response.json()).data;
@@ -17,13 +17,13 @@ export const getMyAccount = async():Promise<any> => {
   return (await response.json()).data[0];
 }
 
-export const getTransferTransaction = async(sender:string, recipient:string):Promise<any> => {
+export const getTransferTransaction = async (sender: string, recipient: string): Promise<any> => {
   try {
     const response = await fetch(`${API_MY_URL[NETWORK]}/transactions?senderAddress=${sender}&recipientAddress=${recipient}&limit=1&offset=0`);
     const data = (await response.json()).data;
     if (data) return data;
 
-  } catch(err) {
+  } catch (err) {
     console.error(`[API ERROR] /transactions`);
   }
 
@@ -32,13 +32,33 @@ export const getTransferTransaction = async(sender:string, recipient:string):Pro
   return (await response.json()).data;
 }
 
-export const getForgedBlocks = async():Promise<any> => {
+export const isTargetTransfer = async (sender: string, recipient: string): Promise<boolean> => {
+  try {
+    const response = await fetch(`${API_MY_URL[NETWORK]}/transactions?senderAddress=${sender}&recipientAddress=${recipient}&includePending=true&limit=1&offset=0`);
+    const data = (await response.json()).data;
+    if (!data) return true;
+    if (data[0].isPending) return false;
+    return new Date(data[0].block.timestamp * 1000).toLocaleDateString("ja-JP") !== new Date().toLocaleDateString("ja-JP");
+
+  } catch (err) {
+    console.error(`[API ERROR] /transactions (isTargetTransfer)`);
+  }
+
+  // retry
+  const response = await fetch(`${API_RETRY_URL[NETWORK]}/transactions?senderAddress=${sender}&recipientAddress=${recipient}&includePending=true&limit=1&offset=0`);
+  const data = (await response.json()).data;
+  if (!data) return true;
+  if (data[0].isPending) return false;
+  return new Date(data[0].block.timestamp * 1000).toLocaleDateString("ja-JP") !== new Date().toLocaleDateString("ja-JP");
+}
+
+export const getForgedBlocks = async (): Promise<any> => {
   try {
     const response = await fetch(`${API_MY_URL[NETWORK]}/blocks?generatorUsername=${DELEGATE.NAME}&offset=0&limit=1`);
     const json = await response.json();
     if (json.meta) return json.meta.total;
 
-  } catch(err) {
+  } catch (err) {
     console.error(`[API ERROR] /blocks`);
   }
 
@@ -49,49 +69,49 @@ export const getForgedBlocks = async():Promise<any> => {
   return 0;
 }
 
-export const getVotesReceived = async():Promise<any> => {
-  const data = { votes: new Array()}
+export const getVotesReceived = async (): Promise<any> => {
+  const data = { votes: new Array() }
   return await getVotesReceivedNext(data, 0);
 }
 
-const getVotesReceivedNext = async(data:{ votes: Array<any>}, offset:number):Promise<any> => {
+const getVotesReceivedNext = async (data: { votes: Array<any> }, offset: number): Promise<any> => {
   const response = await fetch(`${API_MY_URL[NETWORK]}/votes_received?username=${DELEGATE.NAME}&aggregate=true&limit=100&offset=${offset}`);
   const json = await response.json();
   if (!json.data.votes) return data;
   for (const vote of json.data.votes) data.votes.push(vote);
-  if (data.votes.length < json.meta.total) await getVotesReceivedNext(data, offset+100);
+  if (data.votes.length < json.meta.total) await getVotesReceivedNext(data, offset + 100);
   return data;
 }
 
-const getNetworkId = async():Promise<string> => {
+const getNetworkId = async (): Promise<string> => {
   try {
-    const response = await fetch(`${API_MY_URL[NETWORK].slice( 0, -3 )}/status`);
+    const response = await fetch(`${API_MY_URL[NETWORK].slice(0, -3)}/status`);
     return (await response.json()).networkId;
 
-  } catch(err) {
+  } catch (err) {
     console.error(`[API ERROR] /status`);
   }
-  
+
   // retry
-  const response = await fetch(`${API_RETRY_URL[NETWORK].slice( 0, -3 )}/status`);
+  const response = await fetch(`${API_RETRY_URL[NETWORK].slice(0, -3)}/status`);
   return (await response.json()).networkId;
 }
 
-const getSchemas = async(moduleAssetId:string):Promise<any> => {
+const getSchemas = async (moduleAssetId: string): Promise<any> => {
   try {
     const response = await fetch(`${API_MY_URL[NETWORK]}/transactions/schemas?moduleAssetId=${moduleAssetId}`);
     return (await response.json()).data[0];
 
-  } catch(err) {
+  } catch (err) {
     console.error(`[API ERROR] /transactions/schemas`);
   }
-  
+
   // retry
   const response = await fetch(`${API_RETRY_URL[NETWORK]}/transactions/schemas?moduleAssetId=${moduleAssetId}`);
   return (await response.json()).data[0];
 }
 
-export const sendTransaction = async(tx:any, assetSchema:any, isTrasnfer:boolean) => {
+export const sendTransaction = async (tx: any, assetSchema: any, isTrasnfer: boolean) => {
   // Get: Delegate account
   const account = await getMyAccount();
   if (!account) return;
@@ -101,18 +121,18 @@ export const sendTransaction = async(tx:any, assetSchema:any, isTrasnfer:boolean
   if (!networkIdentifier) return;
 
   // Set: Options
-  const options = DELEGATE.MULTISIG? {numberOfSignatures: account.keys.numberOfSignatures}: {};
+  const options = DELEGATE.MULTISIG ? { numberOfSignatures: account.keys.numberOfSignatures } : {};
 
   // Set: MinFee
   tx.fee = transactions.computeMinFee(assetSchema.schema, tx, options);
-  if(isTrasnfer) tx.asset.amount = tx.asset.amount - tx.fee;
+  if (isTrasnfer) tx.asset.amount = tx.asset.amount - tx.fee;
 
   // Sign: Transaction
   if (DELEGATE.MULTISIG) {
     // Set: MultisignatureKeys
     const multisignatureKeys = {
-      mandatoryKeys: account.keys.mandatoryKeys.map((key: string) => {return cryptography.hexToBuffer(key)})||[],
-      optionalKeys: account.keys.optionalKeys.map((key: string) => {return cryptography.hexToBuffer(key)})||[],
+      mandatoryKeys: account.keys.mandatoryKeys.map((key: string) => { return cryptography.hexToBuffer(key) }) || [],
+      optionalKeys: account.keys.optionalKeys.map((key: string) => { return cryptography.hexToBuffer(key) }) || [],
     }
 
     for (const passphrae of DELEGATE.PASSPHRASE) {
@@ -136,7 +156,7 @@ export const sendTransaction = async(tx:any, assetSchema:any, isTrasnfer:boolean
 
   // Send: Transaction
   const payload = cryptography.bufferToHex(transactions.getBytes(assetSchema.schema, tx));
-  const res = await fetch(`${API_MY_URL[NETWORK]}/transactions?transaction=${payload}`,{
+  const res = await fetch(`${API_MY_URL[NETWORK]}/transactions?transaction=${payload}`, {
     method: 'POST',
     headers: {
       'Accept': 'application/json'
@@ -146,7 +166,7 @@ export const sendTransaction = async(tx:any, assetSchema:any, isTrasnfer:boolean
   console.log(json.transactionId);
 }
 
-export const transfer = async(nonce:string, recipientAddress:string, amount:string, message:string):Promise<boolean> => {
+export const transfer = async (nonce: string, recipientAddress: string, amount: string, message: string): Promise<boolean> => {
   try {
     // Get: Schema
     const assetSchema = await getSchemas('2:0');
@@ -172,13 +192,13 @@ export const transfer = async(nonce:string, recipientAddress:string, amount:stri
 
     return true;
 
-  } catch(err) {
+  } catch (err) {
     console.error(err);
     return false;
   }
 }
 
-export const delegateVote = async(nonce:string, recipientAddress:string, amount:string):Promise<boolean> => {
+export const delegateVote = async (nonce: string, recipientAddress: string, amount: string): Promise<boolean> => {
   try {
     // Get: Schema
     const assetSchema = await getSchemas('5:1');
@@ -193,7 +213,7 @@ export const delegateVote = async(nonce:string, recipientAddress:string, amount:
       signatures: [],
       senderPublicKey: cryptography.getPrivateAndPublicKeyFromPassphrase(DELEGATE.PASSPHRASE[0]).publicKey,
       asset: {
-        votes :[
+        votes: [
           {
             delegateAddress: cryptography.getAddressFromLisk32Address(recipientAddress),
             amount: BigInt(amount)
@@ -207,7 +227,7 @@ export const delegateVote = async(nonce:string, recipientAddress:string, amount:
 
     return true;
 
-  } catch(err) {
+  } catch (err) {
     console.error(err);
     return false;
   }
